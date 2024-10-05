a8a3105a0e975fccc427e1a3d6eefd5fbd900bbd
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-05 11:46:54 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace KeyBlockingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape || e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                MessageBox.Show("Клавиша заблокирована.");
+            }
+
+            if (e.KeyCode == Keys.E)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
